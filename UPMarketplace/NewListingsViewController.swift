1a3adfc5cdc4f22b516747017c82a9d83b29fbd3
Krismy <krismy93@gmail.com>
2014-11-15 14:27:13 -0800

    working on listing cell

---
+++ UPMarketplace/NewListingsViewController.swift
@@ -0,0 +1,10 @@
+//
+//  NewListingsViewController.swift
+//  UPMarketplace
+//
+//  Created by Krismy on 11/15/14.
+//  Copyright (c) 2014 UP Marketplace. All rights reserved.
+//
+
+import UIKit
+import ListingCollectionView
