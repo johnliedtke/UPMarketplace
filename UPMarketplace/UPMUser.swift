bf77d208a4dad39748dafd5d5eb5fce4a9e9233c
John Liedtke <John@Johns-MacBook-Pro.local>
2015-01-14 12:30:23 -0800

    Adde UPMUser

---
+++ UPMarketplace/UPMUser.swift
@@ -0,0 +1,13 @@
+//
+//  UPMUser.swift
+//  UPMarketplace
+//
+//  Created by John Liedtke on 1/14/15.
+//  Copyright (c) 2015 UP Marketplace. All rights reserved.
+//
+
+import UIKit
+
+class UPMUser: PFUser {
+   
+}
