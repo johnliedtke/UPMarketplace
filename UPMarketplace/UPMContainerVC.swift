3583ec4f56b95b36bd170f919a391543aa0a996f
John Liedtke <John@Johns-MacBook-Pro.local>
2015-01-28 18:32:33 -0800

    Completed container class.

--- UPMarketplace/UPMContainerVC.swift
+++ UPMarketplace/UPMContainerVC.swift
@@ -8,28 +8,145 @@
 
 import UIKit
 
-class UPMContainerVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+/**
+A view controller with extended functionality for easily managing
+child view controllers.
+*/
+public class UPMContainerVC: UIViewController {
+  
+  // MARK: - Public Properties
+  
+  /// Where you want to display the child controller's view
+  var containerView: UIView!
+  
+  /// Enable left and right transitions
+  var isLeftRightTransition = true
+  
+  /// Duration of changing container controllers
+  var transitionDuration = 0.45
+  
+  /// Visible view controller in containerView
+  private(set) public var currentViewController: UIViewController!
+  
+  // MARK: - Private Properties
+  
+  /// Holds co
+  private var containerViewControllers = [UIViewController]()
+  
+  /// Is the controller in a state of changing child controllers
+  private var isTransitioning = false
+  
+  // MARK: - Public Methods
+  
+  /**
+  MUST OVERRIDE
+  Initialize all container controllers and return them along with the view to be used
+  for the container view. Method gets called in viewDidLoad() once.
+  - Do not call this method on your own.
+  - Other customization may be done here, e.g. adjusting transitionDuration
+  
+  :returns: containerView View to display view controllers
+  :returns: controllers View controllers to display in containerView
+  */
+  public func setUpViewControllers() -> (containerView: UIView, controllers: [UIViewController]) {
+    
+    return (UIView(), [UIViewController]())
+  }
+  
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+    // Load child view controllers
+    containerView = setUpViewControllers().containerView
+    containerViewControllers = setUpViewControllers().controllers
+    
+    // Add initial controller
+    currentViewController = containerControllerAtIndex(0)
+    addChildViewController(currentViewController)
+    containerView.addSubview(currentViewController.view)
+    currentViewController.didMoveToParentViewController(self)
+    
+  }
+  
+  /**
+  Transitions to the next view controller in a "circular way".
+  */
+  public func nextViewController() {
+    let currentIndex = currentControllerIndex()
+    
+    if currentIndex+1 >= containerViewControllers.count {
+      transitionToViewControllerAtIndex(0)
+    } else {
+      transitionToViewControllerAtIndex(currentIndex+1)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  /**
+  Returns the index of the visible view controller.
+  
+  :returns: index of current view controller
+  */
+  public func currentControllerIndex() -> Int {
+    for (index, vc) in enumerate(containerViewControllers) {
+      if vc.isEqual(currentViewController) {
+        return index
+      }
+    }
+    return 0
+  }
+  
+  /**
+  Change the view of the containerView to display the view of the controlle at
+  the index of containerViewControllers.
+  
+  :param: index Index of view controller to display
+  */
+  public func transitionToViewControllerAtIndex(index: Int) {
+    
+    var nextViewController: UIViewController! = (containerControllerAtIndex(index))
+    
+    if nextViewController == nil || isTransitioning {
+      return
+    }
+    isTransitioning = true
+    
+    addChildViewController(nextViewController)
+    currentViewController.willMoveToParentViewController(nil)
+    
+    // Throw the nextViewController to the left/right for transition
+    let height = CGRectGetHeight(containerView.frame)
+    let width = CGRectGetHeight(containerView.frame)
+    
+    if isLeftRightTransition {
+      var currentIndex = currentControllerIndex()
+      if currentIndex > index && index - 1 != containerViewControllers.count {
+        nextViewController.view.frame = CGRectMake(-width, 0, width, height)
+      } else {
+        nextViewController.view.frame = CGRectMake(width, 0, width, height)
+      }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    transitionFromViewController(currentViewController, toViewController: nextViewController, duration: transitionDuration, options: nil, animations: { () -> Void in
+      
+      self.currentViewController.view.frame = CGRectMake(0-width, 0, width, height)
+      nextViewController.view.frame = CGRectMake(0, 0, width, height)
+      
+      }) { (finished) -> Void in
+        
+        self.currentViewController.removeFromParentViewController()
+        nextViewController.didMoveToParentViewController(self)
+        self.currentViewController = nextViewController
+        self.isTransitioning = false
     }
-    */
-
+  }
+  
+  // MARK: - Private Methods
+  
+  private func containerControllerAtIndex(index: Int) -> UIViewController? {
+    if index < 0 && index > containerViewControllers.count {
+      return nil
+    }
+    return containerViewControllers[index]
+    
+  }
 }
+
