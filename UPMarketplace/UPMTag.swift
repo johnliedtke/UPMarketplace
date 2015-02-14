36081d25c6a0d1453600397e3e17e68dbde1bc9e
woundeddophin <justfrznh2o@gmail.com>
2015-02-14 13:15:18 -0800

    Adds enum for Tags

--- UPMarketplace/UPMTag.swift
+++ UPMarketplace/UPMTag.swift
@@ -6,4 +6,37 @@
 //  Copyright (c) 2015 UP Marketplace. All rights reserved.
 //
 
+public enum UPMTag {
+  
+  /// Category for filtering the different other listings.
+  case Buisness
+  case Engineering
+  case CAS
+  case Nursing
+  case Education
+  case Core
+  
+  case New
+  case Used
+  case LikeNew
+
+  case Free
+
+  case OneHundred
+  case TwoHundred
+  case ThreeHundred
+  case FourHundred
+  case FiveHundred
+  
+  case Large
+  case SomeAssemblyRequired
+  
+  case EE
+  case ME
+  case CE
+  case CS
+
+}
+
+
 
